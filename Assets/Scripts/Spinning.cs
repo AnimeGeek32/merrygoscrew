265a2289e8f10de4502375579fd72c38c337480b
Daniel Jung <djanimelover18@gmail.com>
2012-06-16 16:54:45 -0400

    Added swiping sound in the Main scene.

--- Assets/Scripts/Spinning.cs
+++ Assets/Scripts/Spinning.cs
@@ -36,10 +36,12 @@ public class Spinning : MonoBehaviour {
 			if(direction < 0) {
 				//Swipe Left
 				iTween.MoveBy(Character,iTween.Hash("x", -moveDistance));
+				audio.Play();
 				Debug.Log("MoveLeft");
 			} else {
 				//Swipe Right
 				iTween.MoveBy(Character,iTween.Hash("x", moveDistance));
+				audio.Play();
 				Debug.Log("MoveRight");
 	
 	}
