9810286e2d8d25b2891cae346ce0ab786fd323bf
Tim Cochrane <tim@bombingbrain.com>
2012-06-16 18:50:32 -0400

    WTF?

--- Assets/Scripts/CollisionLogic.cs
+++ Assets/Scripts/CollisionLogic.cs
@@ -2,27 +2,32 @@ using UnityEngine;
 using System.Collections;
 
 public class CollisionLogic : MonoBehaviour {
+	bool isGathered=false;
 	
 	
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		 
 	}
 	void OnTriggerEnter(Collider collision) //when you hit the acorn
 	{
-		if(collision.gameObject.tag == "Acorn") {
-			Debug.Log("GOT IT");
+		
+		if((collision.gameObject.tag == "Player") && (!isGathered)) {
+			isGathered=true;
+			
 			GameObject staminaMeterManager = GameObject.Find("StaminaMeter");
 			int currentStaminaLevel = staminaMeterManager.GetComponent<StaminaMeterManager>().getStaminaLevel();
 			if(currentStaminaLevel < 9)
 			{
 				staminaMeterManager.GetComponent<StaminaMeterManager>().setStaminaLevel(currentStaminaLevel + 1);
 			}
+			Destroy(this.gameObject);
 		}
 	}
 	
